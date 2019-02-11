e940da6024383e19e35e4bbd33c1ce40c9e8c339
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-02-11 14:52:36 +0300

    chore: add and configure canjs. Hooray! It's working :)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import 'canjs/amd/can';
+import 'can/construct/super';
+import 'can/construct/proxy';
+import 'can/control/plugin';
+import 'can/list/sort';
+import 'can/map/attributes';
+import 'can/map/backup';
+import 'can/map/validations';
+
+console.log(can);
 console.log('hello world');
 document.querySelector('h1').innerText = 'Hello world from js';
--- webpack.js
+++ webpack.js
@@ -2,10 +2,13 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const path = require('path');
 
 
 // it's just test project for myself, no such thing as production here
 // that's why it's only development settings here
+
+const nodeModulesDir = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
   mode: 'development',
@@ -14,6 +17,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.json'],
+    modules: [nodeModulesDir],
+    alias: {
+      can: 'canjs/amd/can/',
+    },
   },
   devtool: 'cheap-module-source-map',
   devServer: {
@@ -30,6 +37,11 @@ module.exports = {
     }),
     new ErrorOverlayPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
   ],
   module: {
     rules: [
@@ -43,6 +55,18 @@ module.exports = {
       {
         test: /\.(png|jpg|gif|svg|eot|woff|woff2|ttf|mp3)$/,
         use: 'file-loader',
+      },
+      {
+        test: /\.stache/,
+        loader: 'raw-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        },
       },
     ],
   },
