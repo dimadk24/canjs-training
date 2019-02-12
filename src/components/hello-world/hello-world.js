ec5e8f109e54c0fe54bffa973c25cb2e969ba557
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-02-12 12:20:01 +0300

    feat: starting working with events

--- src/components/hello-world/hello-world.js
+++ src/components/hello-world/hello-world.js
@@ -1,9 +1,13 @@
-import template from './hello-world.stache';
+
+const viewModel = can.Map.extend({
+  name: 'World',
+  sayHello: function sayHello() {
+    console.log(`hello ${this.attr('name')}`);
+  },
+});
 
 export default can.Component.extend({
   tag: 'hello-world',
-  template,
-  viewModel: {
-    name: 'World',
-  },
+  template: can.view('/src/components/hello-world/hello-world.stache'),
+  viewModel,
 });
